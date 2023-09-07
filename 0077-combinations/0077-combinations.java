0a3e69625dd8ef1b355dac41ceb47e22885dbb12
GUNTUKU NARESH <94747836+nareshyadav2912@users.noreply.github.com>
2023-09-07 18:18:44 +0530

    Time: 20 ms (23.05%), Space: 92.9 MB (5.26%) - LeetHub

--- 0077-combinations/0077-combinations.java
+++ 0077-combinations/0077-combinations.java
@@ -1,15 +1,16 @@
 class Solution {
     List<List<Integer>> ans=new ArrayList<>();
     public void helper(int num,int n,int k,List<Integer> ds){
-        if(k==0){
-            ans.add(new ArrayList<>(ds));
+        if(num>n){
+            if(k==0){
+                ans.add(new ArrayList<>(ds));
+            }
             return;
         }
-        for(int i=num;i<=n-k+1;i++){
-            ds.add(i);
-            helper(i+1,n,k-1,ds);
-            ds.remove(ds.size()-1);
-        }
+        ds.add(num);
+        helper(num+1,n,k-1,ds);
+        ds.remove(ds.size()-1);
+        helper(num+1,n,k,ds);
     }
     public List<List<Integer>> combine(int n, int k) {
         helper(1,n,k,new ArrayList<>());
